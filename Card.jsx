090eba01f0fccb69d97a49d9223bb94b39d34f0d
Aashutosh Bhatt <aashu1112@gmail.com>
2017-06-03 20:21:23 +0530

    Added code for responsive UI.

--- Card.jsx
+++ Card.jsx
@@ -6,7 +6,12 @@ class Card extends React.Component {
   render() {
     const module = require(`./cards/${this.props.cardId}.jsx`)
     return (
-      <div  id='question_card' className={this.props.cardOrderId === 0 ? 'question-card active' : 'question-card'} data-order={this.props.cardOrderId} style={this.props.cardStyle} >
+      <div
+        id='question_card'
+        className={this.props.cardOrderId === 0 ? 'question-card active' : 'question-card'}
+        data-order={this.props.cardOrderId}
+        style={this.props.cardStyle}
+        data-card-type={this.props.cardType}>
         { module.render(this.props, this.state) }
       </div>
     )
--- Container.jsx
+++ Container.jsx
@@ -11,60 +11,130 @@ export class Container extends React.Component {
     this.state = {
       card_meta_data: [],
       card_data: [],
-      device_data: undefined
+      device_data: undefined,
+      total_questions: 0,
+      card_height: 300
     };
   }
 
-
   componentDidMount() {
+
     axios.all([axios.get(this.props.containerURL), axios.get(this.props.dataURL)])
       .then(axios.spread((cont, card) => {
         this.setState({
           card_meta_data: cont.data.cards,
           card_data: card.data.root.row,
-          device_data : cont.data.platforms
+          device_data : cont.data.platforms,
+          total_questions: card.data.root.row.length
         });
+
+        // const main_container = document.querySelector(".main-container");
+        // const card_stack = document.querySelector(".card-stack"),
+        //   intro_card = document.querySelector(".question-card[data-card-type='intro']"),
+        //   conclusion_card = document.querySelector(".question-card[data-card-type='score']"),
+        //   questions = document.querySelectorAll(".question-card"),
+        //   next = document.querySelector('#next'),
+        //   reset = document.querySelector('#reset'),
+        //   credits = document.querySelector('#credits'),
+        //   question_cards = document.querySelectorAll(".question-card:not([data-order='" + (this.state.card_data.length - 1) + "']) .back"),
+        //   question_cards_length = question_cards.length,
+        //   config = {},
+        //   width = (window.innerWidth - 20),
+        //   margin_left = (-(window.innerWidth - 20) / 2);
+
+        // let card_height = this.state.card_height;
+        // main_container.style.width = (window.innerWidth - 14) + "px";
+        // card_stack.style.width = width + "px";
+        // card_stack.style.marginLeft = margin_left + "px";
+        // intro_card.style.width = width + "px";
+        // intro_card.style.marginLeft = margin_left + "px";
+
+        // card_height = this.getCardHeight(config, card_height, questions, this.total_questions);
+        // main_height = ((this.total_questions + 1) * 24) + card_height + 70;
+
+        // if(window.innerWidth <= 500) {
+        //   main_height -= 140;
+        // } else {
+        //   main_height -= 75;
+        // }
+
+        // card_stack.style.height = card_height + "px";
+        // intro_card.style.height = card_height + "px";
+
+        // if(config.quiz_type === "scoring") {
+        //   conclusion_card.style.height = card_height + "px";
+        //   conclusion_card.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0, 0, " + ((total_questions + 1) * 320 * -1) + ", " + (1 + 0.16 * (total_questions + 1)) + ")";
+        // }
+
+        // main_container.style.height = main_height + "px";
+        // for(let i = 0; i < questions.length; i++) {
+        //   questions[i].style.height = card_height + "px";
+        // }
+
+        // if(window.innerWidth <= 500) {
+        //   let help_text = generateHelpText(config, total_questions, language_texts);
+
+        //   help_text.style.top = reset.style.top = next.style.top = card_height + (total_questions * 24) + 15 + "px";
+        //   credits.style.top = card_height + (total_questions * 24) + 65 + "px";
+        // } else {
+          // reset.style.top = next.style.top = card_height + (this.total_questions * 24) + 15 + "px";//(total_questions * 24) + 300 + 15 + "px";
+          // credits.style.top = card_height + (this.total_questions * 24) + 65 + "px";
+        // }
+
       }));
   }
 
-  getScreenSize() {
-    let w = window,
-      d = document,
-      e = d.documentElement,
-      g = d.getElementsByTagName('body')[0],
-      width = w.innerWidth || e.clientWidth || g.clientWidth,
-      height = w.innerHeight|| e.clientHeight|| g.clientHeight;
+  getCardHeight(config, card_height, questions, total_questions) {
+    let max_height = card_height,
+      intro_header_bcr = document.querySelector(".intro-header").offsetHeight,
+      intro_desc_bcr = document.querySelector(".intro-description").offsetHeight,
+      intro_button_bcr = document.querySelector(".intro-button").offsetHeight,
+      intro_height = (intro_header_bcr + intro_desc_bcr + intro_button_bcr + 50);
 
-    return {
-      width: width,
-      height: height
-    };
+    if(intro_height > max_height) {
+      max_height = intro_height;
+    }
+
+    // if(!(config.quiz_type === "scoring" && config.flip_card === "no")) {
+    //   let max_back = document.querySelector(".max-content"),
+    //     b_title = max_back.querySelector(".title").offsetHeight,
+    //     b_ans = max_back.querySelector(".correct-answer").offsetHeight,
+    //     b_gif = max_back.querySelector(".gif-div").offsetHeight,
+    //     b_desc = max_back.querySelector(".answer").offsetHeight,
+    //     b_fact = max_back.querySelector(".fact").offsetHeight,
+    //     b_num = max_back.querySelector(".question-number").offsetHeight,
+    //     back_height = b_title + Math.max(b_ans, 100) + b_desc + b_fact + b_num + 25;
+
+    //   if(back_height > max_height) {
+    //     max_height = back_height;
+    //   }
+    // }
+
+    for(let i = 0; i < questions.length; i++) {
+      let q_title = questions[i].querySelector(".title").offsetHeight,
+        q_options = questions[i].querySelectorAll(".option-div"),
+        q_num = questions[i].querySelector(".question-number").offsetHeight,
+        q_que = questions[i].querySelector(".question").offsetHeight,
+        q_height,
+        order_id = +questions[i].getAttribute("data-order");
+
+      q_height = q_title + q_que + q_num + 45;
+
+      for(let j = 0; j < q_options.length; j++) {
+        q_height += (q_options[j].offsetHeight + 15);
+      }
+
+      if(q_height > max_height) {
+        max_height = q_height;
+      }
+      questions[i].style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0, " + ((total_questions - order_id) * 24) + ", " + ((order_id + 1) * 320 * -1) + ", " + (1 + 0.16 * (order_id + 1)) + ")";
+    }
+
+    if(max_height > card_height) {
+      card_height = max_height;
+    }
+    return card_height;
   }
-
-  // startQuiz(e) {
-  //   console.log(e, this, "Start Quiz");
-  //   let first_q_card = document.querySelector(".question-card[data-order='1']");
-  //   let total_questions = 8;
-  //   e.target.closest(".intro-card").style.left = (window.innerWidth + 500) + "px";
-
-  //   // if(!(config.quiz_type === "scoring" && config.flip_card === "no")) {
-  //   //   first_q_card.querySelector(".back").style.display = "none";
-  //   // }
-  //   first_q_card.classList.add("active");
-  //   first_q_card.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0, " + ((total_questions) * 24) + ", 0, 1)";
-  //   for(let i = 2; i < total_questions; i++) {
-  //     let card = document.querySelector(".question-card[data-order='" + i + "']"),
-  //       position = card.getAttribute("data-order");
-
-  //     card.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0, " + (((total_questions) - position) * 24) + ", " + (position * 320 * -1) + ", " + (1 + 0.16 * position) + ")";
-  //   }
-  //   // if(config.quiz_type === "scoring") {
-  //   //   let conclusion_card = document.querySelector(".conclusion-card");
-  //   //   conclusion_card.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0, 0, " + (total_questions * 320 * -1) + ", " + (1 + 0.16 * total_questions) + ")";
-  //   // }
-
-
-  // }
 
   startQuiz(e) {
     let q_card = document.querySelector(".question-card.active"),
@@ -104,6 +174,7 @@ export class Container extends React.Component {
 
   optionClicked(e) {
     console.log(e, this, "Option Clicked");
+    this.setState({total_questions: 110});
     let q_card = e.target.closest(".question-card"),
       parent = e.target.closest(".content"),
       back_div,
@@ -201,19 +272,19 @@ export class Container extends React.Component {
       z = 1,
       cards;
 
-    if (this.state.device_data !== undefined) {
-      let dimension = this.getScreenSize();
-      if (dimension.width <= 500) { // mobile
-        styles.width = this.state.device_data.mobile.width;
-        styles.height = this.state.device_data.mobile.height;
-      } else if (dimension.width <= 1024) { //ipad
-        styles.width = this.state.device_data.tablet.width;
-        styles.height = this.state.device_data.tablet.height;
-      } else { // desktop or default
-        styles.width = this.state.device_data.desktop.width;
-        styles.height = this.state.device_data.desktop.height;
-      }
-    }
+    // if (this.state.device_data !== undefined) {
+    //   let dimension = this.getScreenSize();
+    //   if (dimension.width <= 500) { // mobile
+    //     styles.width = this.state.device_data.mobile.width;
+    //     styles.height = this.state.device_data.mobile.height;
+    //   } else if (dimension.width <= 1024) { //ipad
+    //     styles.width = this.state.device_data.tablet.width;
+    //     styles.height = this.state.device_data.tablet.height;
+    //   } else { // desktop or default
+    //     styles.width = this.state.device_data.desktop.width;
+    //     styles.height = this.state.device_data.desktop.height;
+    //   }
+    // }
 
     cards = this.state.card_meta_data.map((card, i) => {
       const style = {};
