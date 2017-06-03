0c241028adc8cecc91405473ffc272db515b893a
Aashutosh Bhatt <aashu1112@gmail.com>
2017-06-03 11:58:13 +0530

    Fixed container functionality.

--- Container.jsx
+++ Container.jsx
@@ -40,49 +40,55 @@ export class Container extends React.Component {
     };
   }
 
+  onClickOfNext(e) {
+    e.target.parentElement.style.left = "-1000px"
+  }
+
 
   render() {
-    let styles = {};
-    if (this.state.device_data !== undefined){
-      let dimension = this.getScreenSize();
-      console.log(dimension.width, "width")
-      if (dimension.width <= 500){ // mobile
-        styles.base = {
-          width : this.state.device_data.mobile.width,
-          height : this.state.device_data.mobile.height         
-        }
-      } else if (dimension.width <= 1024){ //ipad
-        styles.base = {
-          width : this.state.device_data.tablet.width,
-          height : this.state.device_data.tablet.height
-        }
-      } else { // desktop or default
-        styles.base = {
-          width : this.state.device_data.desktop.width,
-          height : this.state.device_data.desktop.height         
-        }
-      }
-    }
-    let i = 0,
+    let styles = {},
       x = this.state.card_meta_data.length * 24,
       y = 0,
-      z = 1;
-    let cards = this.state.card_meta_data.map((card) => {
-      console.log(this.state.card_meta_data.length - i, "zindex")
-      styles.custom = {
-        zIndex : this.state.card_meta_data.length - i,
-        transform : "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0," + x + "," + y + "," + z + ")"
+      z = 1,
+      cards;
+
+    if (this.state.device_data !== undefined) {
+      let dimension = this.getScreenSize();
+      if (dimension.width <= 500) { // mobile
+        styles.width = this.state.device_data.mobile.width;
+        styles.height = this.state.device_data.mobile.height;
+      } else if (dimension.width <= 1024) { //ipad
+        styles.width = this.state.device_data.tablet.width;
+        styles.height = this.state.device_data.tablet.height;
+      } else { // desktop or default
+        styles.width = this.state.device_data.desktop.width;
+        styles.height = this.state.device_data.desktop.height;
       }
-      i++;
+    }
+
+
+    cards = this.state.card_meta_data.map((card, i) => {
+      const style = {};
+      style.width = styles.width;
+      style.height = styles.height;
+      style.zIndex = this.state.card_meta_data.length - i;
+      style.transform = `matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0.0005, 0, ${x}, ${y}, ${z})`;
+
       x = x - 24;
       y = y - 320;
       z = z + 0.16;
-      console.log(styles, "styles")
-      // return(
-      //   <div key = {card.id} className = "card-div" style = {{...styles.base,...styles.custom}} ></div>
-      // )
-    })
-    return <div>{cards}</div>
+
+      return (
+        <div key = {card.id} className = "card-div" style={style} >
+          <br />
+          <br />
+          <br />
+          <button onClick={(e) => this.onClickOfNext(e)}>Next</button>
+        </div>
+      )
+    });
+
+    return <div className='cards-container'> {cards} </div>
   }
 }
 
